ac94b386c99c79bfbedb9780ec0806b4afe773c7
Raul5304 <raullecinena@iesrioarba.es>
2024-10-28 11:03:58 +0100

    .

--- circulo.py
+++ circulo.py
@@ -1,1 +1,17 @@
 #! /C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+from figura import figura
+
+class circulo(figura):
+    
+    __color = None
+    
+    # TODO: elevar color a la superclase
+    def __init__(self, dimensions=..., color=None) -> None:
+        super().__init__(dimensions)
+        self.__color == color
+
+if __name__ == "__main__":
+    
+    c0 = circulo()
+    print(c0.getDimensiones)
--- figura.py
+++ figura.py
@@ -1,1 +1,38 @@
 #! /C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+class figura:
+    
+    __dimensiones = []
+    
+    def __init__(self, dimensions=[1,1]) -> None:
+        self.__dimensiones = dimensions
+    
+    def getDimensiones(self):
+        return self.__dimensiones
+    
+    def perimetro(self):
+        return 5
+    
+    def area(self):
+        return 5
+
+    def __str__(self) -> str:
+        answ = f"Figura: "
+        answ += f"Area: {self.area()} "
+        answ += f"Perímetro: {self.perimetro()}"
+        return answ
+        
+
+if __name__ == "__main__":
+    
+    f0 = figura()
+    # print(f0.getDimensiones())
+    print(f0)
+    
+    f1 = figura([2])
+    # print(f1.getDimensiones())
+    print(f1)
+    
+    f2 = figura([2,3])
+    # print(f2.getDimensiones())
+    print(f2)
